2e6e1d8bb18d7ae012570b7e264b0a2f368fc3e0
vasu <vasuvekariya123@gmail.com>
2025-10-28 18:07:32 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.send("This is Home Page");
 })
 
+app.get("/order", (req, res) => {
+    res.send("This is Order Page");
+})
+
+app.get("/contact", (req, res) => {
+    res.send("This is Contact Page");
+}  )
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
